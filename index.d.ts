6a444c70bfc86597c27ba2b4999f17e12e89f69b
sofiyabalchinova <114357417+sofiyabalchinova@users.noreply.github.com>
2024-10-03 15:36:13 +0300

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,1 @@
-export * from "./types";
-export * from "./enums";
-export * from "./modifiers";
-export { popperGenerator, detectOverflow, createPopper as createPopperBase } from "./createPopper";
-export { createPopper } from "./popper";
-export { createPopper as createPopperLite } from "./popper-lite";
+export * from './lib';
